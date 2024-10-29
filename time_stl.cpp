90dcd31138d6250bfb0460407251828cec4dcc79
Brandon Braun <brandonbraun653@protonmail.com>
2024-10-29 13:57:09 +0000

    Add STL implementation of time interface with millis, micros, and delay functions

---
+++ time_stl.cpp
@@ -0,0 +1,58 @@
+/******************************************************************************
+ *  File Name:
+ *    time_stl.cpp
+ *
+ *  Description:
+ *    STL implementation of the time interface
+ *
+ *  2024 | Brandon Braun | brandonbraun653@protonmail.com
+ *****************************************************************************/
+
+/*-----------------------------------------------------------------------------
+Includes
+-----------------------------------------------------------------------------*/
+#include <mbedutils/interfaces/time_intf.hpp>
+#include <chrono>
+#include <thread>
+
+namespace mb::time
+{
+  /*---------------------------------------------------------------------------
+  Private Functions
+  ---------------------------------------------------------------------------*/
+
+  static inline size_t absolute_system_time_ms()
+  {
+    return std::chrono::duration_cast<std::chrono::milliseconds>( std::chrono::system_clock::now().time_since_epoch() ).count();
+  }
+
+  /*---------------------------------------------------------------------------
+  Public Functions
+  ---------------------------------------------------------------------------*/
+
+  size_t millis()
+  {
+    static size_t start_time = absolute_system_time_ms();
+
+    return absolute_system_time_ms() - start_time;
+  }
+
+
+  size_t micros()
+  {
+    return millis() * 1000;
+  }
+
+
+  void delayMilliseconds( const size_t val )
+  {
+    std::this_thread::sleep_for( std::chrono::milliseconds( val ) );
+  }
+
+
+  void delayMicroseconds( const size_t val )
+  {
+    std::this_thread::sleep_for( std::chrono::microseconds( val ) );
+  }
+
+}  // namespace mb::time
